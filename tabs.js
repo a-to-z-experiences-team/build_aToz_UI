54f0fbfd18be5fc500823ceb2af87ccc1efb3869
Paul Abernathy <paul.abernathy7@gmail.com>
2019-05-03 00:34:48 -0400

    added js

---
+++ tabs.js
@@ -0,0 +1,48 @@
+class TabLink {
+  constructor(element) {
+    this.element = element
+
+    this.data = element.dataset.tab
+    console.log(this.data)
+
+    this.itemElement = document.querySelector(`.card[data-tab='${this.data}']`)
+    console.log(this.item)
+
+    this.tabItem = new TabItem(this.itemElement);
+
+    this.element.addEventListener('click', () => this.select())
+  };
+
+  select() {
+    const links = document.querySelectorAll(".tab")
+
+    Array.from(links).forEach(link =>
+      link.classList.remove('btn-danger'))
+
+      this.element.classList.add('btn-danger')
+
+      this.tabItem.select()
+  }
+
+}
+ 
+class TabItem {
+  constructor(element) {
+    this.element = element
+  }
+
+  select() {
+    const items = document.querySelectorAll('.card')
+
+    items.forEach(element => element.classList.remove('card-selected'))
+
+    this.element.classList.add('card-selected')
+  }
+}
+
+
+const links = document.querySelectorAll(".tab");
+
+links.forEach(function(link){
+  new TabLink(link)
+}); 
